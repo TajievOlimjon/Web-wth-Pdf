8dd3c45359ff08dbc5e87cf29f308e0da1e6e98f
Olimjon <tajiev0711@gmail.com>
2023-06-09 17:25:26 +0500

    Initial

---
+++ WebApi/Controllers/StudentController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Data;
+using WebApi.Entities;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public StudentController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpGet("GetAllStudents")]
+        public async Task<IActionResult> Get()
+        {
+            var students = await _dbContext.Students.ToListAsync();
+            return Ok(students);
+        }
+
+        // GET api/<StudentController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var student = await _dbContext.Students.FindAsync(id);
+            return Ok(student);
+        }
+
+        // POST api/<StudentController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Student student)
+        {
+            await _dbContext.Students.AddAsync(student);
+            var result = await _dbContext.SaveChangesAsync();
+            if (result == 0) return BadRequest("Data not added !");
+            return BadRequest("Data successfully added !");
+        }
+
+        // PUT api/<StudentController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put([FromBody] Student model)
+        {
+            var student = await _dbContext.Students.FindAsync(model.Id);
+            if (student == null) return NotFound("Data not found !");
+            _dbContext.Students.Update(model);
+            var result = await _dbContext.SaveChangesAsync();
+            if (result == 0) return BadRequest("Data not updated !");
+            return BadRequest("Data successfully updated !");
+        }
+
+        // DELETE api/<StudentController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var student = await _dbContext.Students.FindAsync(id);
+            if (student == null) return NotFound("Data not found !");
+            _dbContext.Students.Remove(student);
+            var result = await _dbContext.SaveChangesAsync();
+            if (result == 0) return BadRequest("Data not removed !");
+            return BadRequest("Data successfully removed !");
+        }
+    }
+}
---
+++ WebApi/Entities/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApi.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = null!;
+        public string? Email { get; set; } = null;
+        public int Age { get; set; }
+        public DateTimeOffset BirthDate { get; set; }
+        public string? Address { get; set; } = null;
+    }
+}
+
